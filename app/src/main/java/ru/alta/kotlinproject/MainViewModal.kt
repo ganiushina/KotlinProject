2950a89b0dc898596587a429dd2c62b1d41085bc
ganiushina <32090008+ganiushina@users.noreply.github.com>
2020-01-31 14:15:23 +0400

    Перенесла счетчик в Модель

--- app/src/main/java/ru/alta/kotlinproject/MainViewModal.kt
+++ app/src/main/java/ru/alta/kotlinproject/MainViewModal.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModal : ViewModel(){
+    var count: Int = 0
     private val viewStateLiveData: MutableLiveData<String> = MutableLiveData()
     init {
         viewStateLiveData.value = "Hello World!!!"
@@ -12,7 +13,8 @@ class MainViewModal : ViewModel(){
 
     fun viewState(): LiveData<String> = viewStateLiveData
 
-    fun updateCount(count: Int){
+    fun updateCount(){
+        ++count
         viewStateLiveData.value = count.toString()
     }
 
